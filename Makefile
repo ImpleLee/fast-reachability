39de32301eba4e7c5ac04e7c2c12e746cbac1f34
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2023-07-21 05:18:50 +0800

    many content

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 CC = g++
-CXXFLAGS = -std=c++17 -g -Og -Wall -Wextra -march=native
+CXXFLAGS = -std=c++17 -g -O3 -Wall -Wextra -march=native
 TARGETS := $(patsubst %.cpp, build/%, $(wildcard *.cpp))
 
-all: $(TARGETS)
+run: all
+	./build/main
 
 build/%: %.cpp build
 	$(CC) $(CXXFLAGS) $< -o $@
@@ -10,6 +11,9 @@ build/%: %.cpp build
 build:
 	mkdir -p build
 
-.PHONY: clean all build
+.PHONY: clean all build run time
+all: $(TARGETS)
+time: all
+	time ./build/main >/dev/null
 clean:
 	rm -rf build
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <bitset>
 #include <array>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <tuple>
 using namespace std;
+
+#define $ if (true)
 
 using coord = signed char[2];
 using mino = coord[4];
@@ -11,8 +18,8 @@ struct block {
   kick kicks[4][3];
 };
 
-template <unsigned N>
-constexpr void remove_range(const bitset<N> &data, unsigned start, unsigned end) {
+template <size_t N>
+constexpr bitset<N> remove_range(const bitset<N> &data, unsigned start, unsigned end) {
   bitset<N> below = data << (N - start);
   below >>= (N - start);
   bitset<N> above = data >> end;
@@ -20,92 +27,573 @@ constexpr void remove_range(const bitset<N> &data, unsigned start, unsigned end)
   return below | above;
 }
 
-template <unsigned W, unsigned H>
+template <int W, int H>
 struct board {
-  using board_t = bitset<W * H>;
+  #define DEF_OPERATOR(ret, op, param, expr) \
+    ret operator op(param) const { \
+      return {data op expr}; \
+    } \
+    ret &operator op##=(param) { \
+      data op##= expr; \
+      return *this; \
+    }
+  #define DEF_BOARD(name) \
+    struct name { \
+      bitset<W * H> data; \
+      name set(size_t i, bool value = true) { \
+        data.set(i, value); \
+        return *this; \
+      } \
+      int get(int x, int y) const { \
+        if (x < 0 || x >= W || y < 0 || y >= H) { \
+          return 2; \
+        } \
+        return data[y * W + x]; \
+      }\
+      bool any() const { \
+        return data.any(); \
+      } \
+      name operator~() const { \
+        return {~data}; \
+      } \
+      bool operator[](size_t i) const { \
+        return data[i]; \
+      } \
+      DEF_OPERATOR(name, &, const name &rhs, rhs.data) \
+      DEF_OPERATOR(name, |, const name &rhs, rhs.data) \
+      DEF_OPERATOR(name, >>, size_t i, i) \
+      DEF_OPERATOR(name, <<, size_t i, i) \
+    }
+  DEF_BOARD(board_t);
+  DEF_BOARD(inv_board_t);
+  #undef DEF_BOARD
+  #undef DEF_OPERATOR
+  friend string to_string(const board_t &board) {
+    string ret;
+    for (int y = H - 1; y >= 0; --y) {
+      for (int x = 0; x < W; ++x) {
+        ret += board.data[y * W + x] ? "[]" : "  ";
+      }
+      ret += '\n';
+    }
+    return ret;
+  }
+  friend string to_string(const inv_board_t &board1, const board_t &board2) {
+    string ret;
+    for (int y = H - 1; y >= 0; --y) {
+      for (int x = 0; x < W; ++x) {
+        bool b1 = board1.data[y * W + x];
+        bool b2 = board2.data[y * W + x];
+        if (b1 && b2) {
+          ret += "%%";
+        } else if (b1) {
+          ret += "..";
+        } else if (b2) {
+          ret += "[]";
+        } else {
+          ret += "  ";
+        }
+      }
+      ret += '\n';
+    }
+    return ret;
+  }
   board_t data;
-  constexpr board() {}
   constexpr int clear_full_lines() {
-    board_t board = data;
+    auto board = data;
     int i = 1;
-    for (; (W >> 1) >= i; i >>= 1) {
+    for (; (W >> 1) >= i; i <<= 1) {
       board &= board >> i;
     }
     board &= board >> (W - i);
     int lines = 0;
     for (int y = 0; y < H; ++y) {
       if (board[y * W]) {
-        data = remove_range(data, (y - lines) * W, (y - lines + 1) * W);
+        data = {remove_range(data.data, (y - lines) * W, (y - lines + 1) * W)};
         ++lines;
       }
     }
     return lines;
   }
-  static constexpr board_t relative_move(board_t temp, coord d) {
-    constexpr const board_t L = [](){
-      board_t data;
-      for (int i = 0; i < H; ++i) {
-        data.set(i * W);
-      }
-      return ~data;
-    }();
-    constexpr const board_t R = [](){
-      board_t data;
-      for (int i = 0; i < H; ++i) {
-        data.set(i * W + W - 1);
-      }
-      return ~data;
-    }();
+  template <bool check = true, bool reverse = false, class board_t>
+  static constexpr board_t move_to_center(board_t board, const coord &d) {
     auto dx = d[0], dy = d[1];
-    if (dy < 0) {
-      temp <<= (-dy) * W;
-    } else if (dy > 0) {
-      temp >>= dy * W;
+    if constexpr (reverse) {
+      dx = -dx;
+      dy = -dy;
     }
-    if (dx < 0) {
-      temp >>= -dx;
-      for (int i = 0; i < -dx; ++i) {
-        temp &= R >> i;
-      }
-    } else if (dx > 0) {
-      temp <<= dx;
-      for (int i = 0; i < dx; ++i) {
-        temp &= L << i;
+    board_t mask;
+    if constexpr (check) {
+      mask = ~board_t();
+      if (dx < 0) {
+        const board_t R = [](){
+          board_t data;
+          for (int i = 0; i < H; ++i) {
+            data.set(i * W);
+          }
+          return ~data;
+        }();
+        for (int i = 0; i < -dx; ++i) {
+          mask &= R >> i;
+        }
+      } else if (dx > 0) {
+        const board_t L = [](){
+          board_t data;
+          for (int i = 0; i < H; ++i) {
+            data.set(i * W + W - 1);
+          }
+          return ~data;
+        }();
+        for (int i = 0; i < dx; ++i) {
+          mask &= L << i;
+        }
       }
     }
-    return temp;
+    int move = dy * W + dx;
+    if (move < 0) {
+      board <<= -move;
+    } else {
+      board >>= move;
+    }
+    if constexpr (check) {
+      board &= mask;
+    }
+    return board;
   }
-  constexpr board_t usable_positions(const mino &mino) const {
-    board_t positions = ~board_t();
+  template <size_t N>
+  constexpr inv_board_t usable_positions(const coord (&mino)[N]) const {
+    inv_board_t positions = ~inv_board_t();
     for (const auto &coord : mino) {
-      positions &= relative_move(~data, coord);
+      positions &= move_to_center(inv_board_t{~data.data}, coord);
     }
     return positions;
   }
-  constexpr board_t landable_positions(const mino &mino) const {
-    board_t positions = usable_positions(mino);
-    board_t temp = board_t();
-    for (const auto &coord : mino) {
-      temp |= relative_move(~data, coord);
+  template <size_t N>
+  bool test_usable(const coord (&mino)[N]) const {
+    auto usable = usable_positions(mino);
+    bool ret = true;
+    for (int x = 0; x < W; ++x) {
+      for (int y = 0; y < H; ++y) {
+        bool pred = usable.get(x, y);
+        bool truth = true;
+        for (const auto &[dx, dy] : mino) {
+          truth &= !data.get(x + dx, y + dy);
+        }
+        if (pred != truth) {
+          $ cout << "error at " << x << ", " << y << endl;
+          ret = false;
+        }
+      }
     }
-    temp <<= W;
-    positions &= ~temp;
-    return positions;
+    return ret;
   }
-  static constexpr array<board_t, 5> kick_positions(const board_t &start, const board_t &end, const kick &kick) {
-    array<board_t, 5> positions;
-    for (int i = 0; i < 5; ++i) {
-      positions[i] = relative_move(end, kick[i]) & start;
+  static constexpr inv_board_t landable_positions(const inv_board_t &usable) {
+    return usable & ~(usable << W);
+  }
+  template <size_t N>
+  bool test_landable(const coord (&mino)[N]) const {
+    auto usable = usable_positions(mino);
+    auto landable = landable_positions(usable);
+    bool ret = true;
+    for (int x = 0; x < W; ++x) {
+      for (int y = 0; y < H; ++y) {
+        bool pred = landable.get(x, y);
+        if (!usable.get(x, y)) {
+          continue;
+        }
+        bool truth = false;
+        for (const auto &[dx, dy] : mino) {
+          truth |= data.get(x + dx, y + dy - 1);
+        }
+        if (pred != truth) {
+          $ cout << "error at " << x << ", " << y << endl;
+          ret = false;
+        }
+      }
     }
-    board_t temp = positions[0];
-    for (int i = 1; i < 5; ++i) {
+    return ret;
+  }
+  template <size_t N>
+  constexpr inv_board_t move_positions(const coord (&mino)[N], const coord &dir) const {
+    inv_board_t positions = usable_positions(mino);
+    return move_to_center(positions, dir) & positions;
+  }
+  template <size_t N>
+  bool test_move(const coord (&mino)[N], const coord &dir) const {
+    auto moved = move_positions(mino, dir);
+    auto usable = usable_positions(mino);
+    auto [dx, dy] = dir;
+    bool ret = true;
+    for (int x = 0; x < W; ++x) {
+      for (int y = 0; y < H; ++y) {
+        bool pred = moved.get(x, y);
+        if (!usable.get(x, y)) {
+          continue;
+        }
+        bool truth = usable.get(x + dx, y + dy) == 1;
+        if (pred != truth) {
+          $ cout << "error at " << x << ", " << y << endl;
+          ret = false;
+        }
+      }
+    }
+    return ret;
+  }
+  template <size_t N>
+  static constexpr array<inv_board_t, N> kick_positions(const inv_board_t &start, const inv_board_t &end, const coord (&kick)[N]) {
+    array<inv_board_t, N> positions;
+    for (size_t i = 0; i < N; ++i) {
+      positions[i] = move_to_center(end, kick[i]) & start;
+    }
+    inv_board_t temp = positions[0];
+    for (size_t i = 1; i < N; ++i) {
       positions[i] &= ~temp;
       temp |= positions[i];
     }
     return positions;
   }
+  template <size_t N>
+  static bool test_kick(const inv_board_t &start, const inv_board_t &end, const coord (&kick)[N]) {
+    auto kicks = kick_positions(start, end, kick);
+    bool ret = true;
+    for (int x = 0; x < W; ++x) {
+      for (int y = 0; y < H; ++y) {
+        int pred = -1;
+        for (size_t i = 0; i < N; ++i) {
+          if (kicks[i].get(x, y)) {
+            pred = i;
+            break;
+          }
+        }
+        int truth = -1;
+        if (start.get(x, y) == 1)
+          for (size_t i = 0; i < N; ++i) {
+            if (end.get(x + kick[i][0], y + kick[i][1]) == 1) {
+              truth = i;
+              break;
+            }
+          }
+        if (pred != truth) {
+          $ cout << "error at " << x << ", " << y << ": should be " << truth << ", but got " << pred << endl;
+          ret = false;
+        }
+      }
+    }
+    return ret;
+  }
+  constexpr array<inv_board_t, 4> binary_bfs(const block &block, const coord &start) const {
+    const array<inv_board_t, 4> usable = [&](){
+      array<inv_board_t, 4> usable;
+      for (int i = 0; i < 4; ++i) {
+        usable[i] = usable_positions(block.minos[i]);
+      }
+      return usable;
+    }();
+    const array<array<array<inv_board_t, 5>, 3>, 4> kicks = [&](){
+      array<array<array<inv_board_t, 5>, 3>, 4> kicks;
+      for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 3; ++j) {
+          kicks[i][j] = kick_positions(usable[i], usable[(i + j + 1) % 4], block.kicks[i][j]);
+        }
+      }
+      return kicks;
+    }();
+    constexpr const coord MOVES[] = {{0, -1}, {-1, 0}, {1, 0}};
+    const array<array<inv_board_t, 3>, 4> moves = [&](){
+      array<array<inv_board_t, 3>, 4> moves;
+      for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 3; ++j) {
+          moves[i][j] = move_positions(block.minos[i], MOVES[j]);
+        }
+      }
+      return moves;
+    }();
+    array<inv_board_t, 4> ret;
+    if (!usable[0][start[1] * W + start[0]]) {
+      return ret;
+    }
+    ret[0].set(start[1] * W + start[0]);
+    bool need_visit[4] = {true, false, false, false};
+    for (bool updated = true; updated;) {
+      updated = false;
+      for (int i = 0; i < 4; ++i) {
+        if (!need_visit[i]) {
+          continue;
+        }
+        need_visit[i] = false;
+        for (int j = 0; j < 3; ++j) {
+          inv_board_t from = ret[i] & moves[i][j];
+          inv_board_t to = move_to_center<false, true>(from, MOVES[j]);
+          inv_board_t det = to & ~ret[i];
+          if (det.any()) {
+            ret[i] |= to;
+            need_visit[i] = true;
+            updated = true;
+          }
+        }
+        for (int j = 0; j < 3; ++j) {
+          inv_board_t to;
+          for (int k = 0; k < 5; ++k) {
+            inv_board_t from = ret[i] & kicks[i][j][k];
+            to |= move_to_center<false, true>(from, block.kicks[i][j][k]);
+          }
+          int target = (i + j + 1) % 4;
+          inv_board_t det = to & ~ret[target];
+          if (det.any()) {
+            ret[target] |= to;
+            need_visit[target] = true;
+            updated = true;
+          }
+        }
+      }
+    }
+    for (int i = 0; i < 4; ++i) {
+      ret[i] &= landable_positions(usable[i]);
+    }
+    return ret;
+  }
+  array<inv_board_t, 4> ordinary_bfs(const block &block, const coord &start) const {
+    const array<inv_board_t, 4> usable = [&](){
+      array<inv_board_t, 4> usable;
+      for (int i = 0; i < 4; ++i) {
+        usable[i] = usable_positions(block.minos[i]);
+      }
+      return usable;
+    }();
+    const array<array<array<inv_board_t, 5>, 3>, 4> kicks = [&](){
+      array<array<array<inv_board_t, 5>, 3>, 4> kicks;
+      for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 3; ++j) {
+          kicks[i][j] = kick_positions(usable[i], usable[(i + j + 1) % 4], block.kicks[i][j]);
+        }
+      }
+      return kicks;
+    }();
+    constexpr const coord MOVES[] = {{0, -1}, {-1, 0}, {1, 0}};
+    const array<array<inv_board_t, 3>, 4> moves = [&](){
+      array<array<inv_board_t, 3>, 4> moves;
+      for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 3; ++j) {
+          moves[i][j] = move_positions(block.minos[i], MOVES[j]);
+        }
+      }
+      return moves;
+    }();
+    array<inv_board_t, 4> ret;
+    if (!usable[0][start[1] * W + start[0]]) {
+      return ret;
+    }
+    ret[0].set(start[1] * W + start[0]);
+    queue<tuple<int, int, int, int>> q;
+    q.emplace(start[0], start[1], 0, 0);
+    int last_times;
+    while (!q.empty()) {
+      auto [x, y, i, times] = q.front();
+      q.pop();
+      for (int j = 0; j < 3; ++j) {
+        auto &[dx, dy] = MOVES[j];
+        if (moves[i][j].get(x, y) && ret[i].get(x + dx, y + dy) != 1) {
+          ret[i].set((y + dy) * W + x + dx);
+          q.emplace(x + dx, y + dy, i, times+1);
+        }
+      }
+      for (int j = 0; j < 3; ++j) {
+        for (int k = 0; k < 5; ++k) {
+          auto &[dx, dy] = block.kicks[i][j][k];
+          if (kicks[i][j][k].get(x, y) && ret[(i + j + 1) % 4].get(x + dx, y + dy) != 1) {
+            ret[(i + j + 1) % 4].set((y + dy) * W + x + dx);
+            q.emplace(x + dx, y + dy, (i + j + 1) % 4, times+1);
+            break;
+          }
+        }
+      }
+      last_times = times;
+    }
+    cout << last_times << endl;
+    for (int i = 0; i < 4; ++i) {
+      ret[i] &= landable_positions(usable[i]);
+    }
+    return ret;
+  }
+  array<inv_board_t, 4> ordinary_bfs_without_binary(const block &block, const coord &start) const {
+    bool my_data[H][W];
+    for (int y = 0; y < H; ++y) {
+      for (int x = 0; x < W; ++x) {
+        my_data[y][x] = data.get(x, y);
+      }
+    }
+    auto in_range = [](int x, int y) {
+      return 0 <= x && x < W && 0 <= y && y < H;
+    };
+    auto usable = [&](int i, int x, int y) {
+      for (const auto &[dx, dy] : block.minos[i]) {
+        if (!in_range(x + dx, y + dy)) return false;
+        if (my_data[y + dy][x + dx]) return false;
+      }
+      return true;
+    };
+    constexpr const coord MOVES[] = {{0, -1}, {-1, 0}, {1, 0}};
+    bool visited[4][H][W];
+    for (int i = 0; i < 4; ++i) {
+      for (int y = 0; y < H; ++y) {
+        for (int x = 0; x < W; ++x) {
+          visited[i][y][x] = false;
+        }
+      }
+    }
+    array<inv_board_t, 4> ret;
+    if (!usable(0, start[0], start[1])) {
+      return ret;
+    }
+    visited[0][start[1]][start[0]] = true;
+    queue<tuple<int, int, int>> q;
+    q.emplace(start[0], start[1], 0);
+    while (!q.empty()) {
+      auto [x, y, i] = q.front();
+      q.pop();
+      for (int j = 0; j < 3; ++j) {
+        auto &[dx, dy] = MOVES[j];
+        if (in_range(x + dx, y + dy) && !visited[i][y + dy][x + dx] && usable(i, x + dx, y + dy)) {
+          visited[i][y + dy][x + dx] = true;
+          q.emplace(x + dx, y + dy, i);
+          if (!usable(i, x + dx, y + dy - 1))
+            ret[i].set((y + dy) * W + x + dx);
+        }
+      }
+      for (int j = 0; j < 3; ++j) {
+        for (int k = 0; k < 5; ++k) {
+          auto &[dx, dy] = block.kicks[i][j][k];
+          auto target = (i + j + 1) % 4;
+          if (in_range(x + dx, y + dy) && !visited[target][y + dy][x + dx] && usable(target, x + dx, y + dy)) {
+            visited[target][y + dy][x + dx] = true;
+            q.emplace(x + dx, y + dy, target);
+            if (!usable(target, x + dx, y + dy - 1))
+              ret[target].set((y + dy) * W + x + dx);
+            break;
+          }
+        }
+      }
+    }
+    return ret;
+  }
 };
 
+bool _ = ios::sync_with_stdio(false);
+#define difficult
 int main() {
-  board<10, 20> b;
+#ifdef difficult
+  vector<string> b_str = {
+    "          ",
+    "          ",
+    "          ",
+    "XXXX  XXX ",
+    "XXXXX XXXX",
+    "      X  X",
+    "          ",
+    "   X      ",
+    "    XX X X",
+    "XXX X     ",
+    "XX  X    X",
+    "X    X    ",
+    "XX  XXXX X",
+    "X    XX   ",
+    "XX XXX    ",
+    "       X  ",
+    "      XXX ",
+    "XX X      ",
+    "X    X    ",
+    "X   X    X",
+    "X  XX X XX",
+    "XX  XXXXXX",
+  };
+#else
+  vector<string> b_str = {
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "   XX     ",
+    "X XX    XX",
+    "XXXX XXXXX",
+    "XXXX   XXX",
+    "XXX     XX",
+    "XXXXXX XXX",
+    "XXXX   XXX",
+    "XXXXX XXXX",
+    "XXXX XXXXX",
+    "XXXX XXXXX"
+  };
+#endif
+  for (auto &s : b_str) {
+    for (auto &c : s) {
+      if (c == 'X') {
+        c = '1';
+      } else {
+        c = '0';
+      }
+    }
+  }
+  string res;
+  for (auto &s : b_str) {
+    res.append(s.rbegin(), s.rend());
+  }
+  board<10, 22> b{bitset<10 * 22>{res}};
+  cout << to_string(b.data);
+  cout << b.clear_full_lines() << endl;
+  block B = {
+    {
+#ifdef difficult
+      {{-1, 1}, {0, 1}, {0, 0}, {1, 0}},   // 0
+      {{1, 1}, {1, 0}, {0, 0}, {0, -1}},   // R
+      {{1, -1}, {0, -1}, {0, 0}, {-1, 0}}, // 2
+      {{-1, -1}, {-1, 0}, {0, 0}, {0, 1}}  // L
+#else
+      {{-1, 0}, {0, 0}, {1, 0}, {0, 1}},   // 0
+      {{0, 1}, {0, 0}, {0, -1}, {1, 0}},   // R
+      {{1, 0}, {0, 0}, {-1, 0}, {0, -1}}, // 2
+      {{0, -1}, {0, 0}, {0, 1}, {-1, 0}}  // L
+#endif
+    },
+    {
+      { // 0
+        {{0, 0}, {-1, 0}, {-1, 1}, {0, -2}, {-1, -2}},  // -> R
+        {{0, 1}, {0, 1}, {0, 1}, {0, 1}, {0, 1}},       // -> 2
+        {{0, 0}, {1, 0}, {1, 1}, {0, -2}, {1, -2}}      // -> L
+      },
+      { // R
+        {{0, 0}, {1, 0}, {1, -1}, {0, 2}, {1, 2}},      // -> 2
+        {{1, 0}, {1, 0}, {1, 0}, {1, 0}, {1, 0}},       // -> L
+        {{0, 0}, {1, 0}, {1, -1}, {0, 2}, {1, 2}}       // -> 0
+      },
+      { // 2
+        {{0, 0}, {1, 0}, {1, 1}, {0, -2}, {1, -2}},     // -> L
+        {{0, -1}, {0, -1}, {0, -1}, {0, -1}, {0, -1}},  // -> 0
+        {{0, 0}, {-1, 0}, {-1, 1}, {0, -2}, {-1, -2}}   // -> R
+      },
+      { // L
+        {{0, 0}, {-1, 0}, {-1, -1}, {0, 2}, {-1, 2}},   // -> 0
+        {{-1, 0}, {-1, 0}, {-1, 0}, {-1, 0}, {-1, 0}},  // -> R
+        {{0, 0}, {-1, 0}, {-1, -1}, {0, 2}, {-1, 2}}    // -> 2
+      }
+    }
+  };
+  size_t l = 0;
+  // for (int _ = 0; _ < 100000; ++_) {
+    bitset<10 * 22> temp;
+    auto binary = b.binary_bfs(B, {4, 20});
+    for (int i = 0; i < 4; ++i) {
+      temp ^= binary[i].data;
+    }
+    l ^= temp.count();
+    l = (l << 1) | (l >> 63);
+  // }
+  cout << l << endl;
 }
