de244021a4f1b4a6e96bca89249f82dc08ea1413
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2024-03-08 01:17:10 +0800

    row-oriented impl!

--- board.hpp
+++ board.hpp
@@ -3,6 +3,7 @@
 #include "utils.hpp"
 #include <limits>
 #include <string_view>
+#include <string>
 #include <bitset>
 #include <array>
 #include <type_traits>
@@ -11,137 +12,125 @@
 #include <experimental/simd>
 
 namespace reachability {
-  template <unsigned W, unsigned H, typename under_t=std::uint64_t>
-    requires
-      std::numeric_limits<under_t>::is_integer
-      && std::is_unsigned_v<under_t>
-      && (std::numeric_limits<under_t>::digits >= W)
+  template <unsigned W, unsigned H>
   struct board_t {
-    static constexpr int under_bits = std::numeric_limits<under_t>::digits;
     static constexpr int width = W;
     static constexpr int height = H;
-    static constexpr int lines_per_under = under_bits / W;
-    static constexpr int used_bits_per_under = lines_per_under * W;
-    static constexpr int num_of_under = (H - 1) / lines_per_under + 1;
-    static constexpr int last = num_of_under - 1;
-    static constexpr int remaining_per_under = under_bits - used_bits_per_under;
-    static constexpr under_t mask = under_t(-1) >> remaining_per_under;
-    static constexpr int remaining_in_last = num_of_under * used_bits_per_under - H * W;
-    static constexpr under_t last_mask = mask >> remaining_in_last;
+    static constexpr int block_size = 8;
+    static constexpr int blocks_per_row = (H - 1) / block_size + 1;
+    using under_t = std::uint8_t;
+    static constexpr int num_of_under = std::bit_ceil(W * blocks_per_row);
     constexpr board_t() {
-      check_invariant();
     }
     constexpr board_t(std::string_view s): board_t(convert_to_array(s)) {
-      check_invariant();
     }
     constexpr board_t(std::array<under_t, num_of_under> d): data{d.data(), std::experimental::element_aligned} {
-      check_invariant();
     }
     static constexpr std::array<under_t, num_of_under> convert_to_array(std::string_view s) {
-      std::array<under_t, num_of_under> data = {};
-      for (std::size_t i = 0; i < last; ++i) {
-        data[i] = convert_to_under_t(s.substr(W * H - (i + 1) * used_bits_per_under, used_bits_per_under));
+      std::string reversed = std::string(s.rbegin(), s.rend());
+      std::array<under_t, num_of_under> data = {0};
+      for (std::size_t y = 0; y < H; ++y) {
+        for (std::size_t x = 0; x < W; ++x) {
+          if (reversed[y * W + x] == 'X') {
+            data[y / block_size * W + x] |= under_t(1) << (y % block_size);
+          }
+        }
       }
-      data[last] = convert_to_under_t(s.substr(0, used_bits_per_under - remaining_in_last));
       return data;
     }
     template <int x, int y>
     constexpr void set() {
-      checking_guard _(*this);
-      data[y / lines_per_under] |= under_t(1) << ((y % lines_per_under) * W + x);
+      data[y / block_size * W + x] |= under_t(1) << (y % block_size);
     }
     template <int x, int y>
     constexpr int get() const {
-      checking_guard _(*this);
       if ((x < 0) || (x >= W) || (y < 0) || (y >= H)) {
         return 2;
       }
-      return data[y / lines_per_under] & (under_t(1) << ((y % lines_per_under) * W + x)) ? 1 : 0;
+      return (data[y / block_size * W + x] >> (y % block_size)) & 1;
     }
     constexpr bool any() const {
-      checking_guard _(*this);
       return *this != board_t{};
     }
     constexpr bool operator!=(const board_t &other) const {
-      checking_guard _(*this), _2(other);
       return any_of(data != other.data);
     }
     constexpr board_t operator~() const {
-      checking_guard _(*this);
       board_t other;
       other.data = mask_board() & ~data;
-      checking_guard _2(other);
       return other;
     }
     constexpr board_t &operator&=(const board_t &rhs) {
-      checking_guard _(*this), _2(rhs);
       data &= rhs.data;
       return *this;
     }
     constexpr board_t operator&(const board_t &rhs) const {
-      checking_guard _(*this), _2(rhs);
       board_t result = *this;
       result &= rhs;
       return result;
     }
     constexpr board_t &operator|=(const board_t &rhs) {
-      checking_guard _(*this), _2(rhs);
       data |= rhs.data;
       return *this;
     }
     constexpr board_t operator|(const board_t &rhs) const {
-      checking_guard _(*this), _2(rhs);
       board_t result = *this;
       result |= rhs;
-      checking_guard _3(result);
       return result;
     }
     constexpr board_t &operator^=(const board_t &rhs) {
-      checking_guard _(*this), _2(rhs);
       data ^= rhs.data;
       return *this;
     }
     constexpr board_t operator^(const board_t &rhs) const {
-      checking_guard _(*this), _2(rhs);
       board_t result = *this;
       result ^= rhs;
-      checking_guard _3(result);
       return result;
     }
     template <coord d, bool check = true>
     constexpr void move_() {
-      checking_guard _(*this);
       constexpr int dx = d[0], dy = d[1];
-      if constexpr (dy == 0) {
-        if constexpr (dx > 0) {
-          data <<= dx;
-          data &= mask_board();
-        } else if constexpr (dx < 0) {
-          data >>= -dx;
-        }
-      } else if constexpr (dy > 0) {
-        constexpr int pad = (dy - 1) / lines_per_under;
-        constexpr int shift = (dy - 1) % lines_per_under + 1;
-        auto not_moved = my_shift<dx, shift>(my_split<pad, true>(data));
-        auto moved = my_shift<dx, shift-lines_per_under>(my_split<pad+1, true>(data));
-        data = (not_moved | moved) & mask_board();
-      } else {
-        constexpr int pad = (-dy - 1) / lines_per_under;
-        constexpr int shift = (-dy - 1) % lines_per_under + 1;
-        auto not_moved = my_shift<dx, -shift>(my_split<pad, false>(data));
-        auto moved = my_shift<dx, lines_per_under-shift>(my_split<pad+1, false>(data));
-        data = (not_moved | moved) & mask_board();
+      if constexpr (dx != 0) {
+        auto original = data;
+        data = data_t([original]<std::size_t i>(std::integral_constant<std::size_t, i>) {
+          constexpr auto column = i / W, row = i % W;
+          if constexpr (column >= blocks_per_row) {
+            return under_t(0);
+          } else if constexpr (dx > 0 && row < dx) {
+            return under_t(0);
+          } else if constexpr (dx < 0 && row >= W + dx) {
+            return under_t(0);
+          } else {
+            return original[i - dx];
+          }
+        });
       }
-      if constexpr (check && dx != 0) {
-        data &= mask_move<dx>();
+      if constexpr (dy < 0) {
+        auto not_moved = data >> -dy;
+        auto moved = data_t([moved = data << (block_size + dy)]<std::size_t i>(std::integral_constant<std::size_t, i>) {
+          if constexpr (i + W < W * blocks_per_row) {
+            return moved[i + W];
+          } else {
+            return under_t(0);
+          }
+        });
+        data = not_moved | moved;
+      } else if constexpr (dy > 0) {
+        auto not_moved = data << dy;
+        auto moved = data_t([moved = data >> (block_size - dy)]<std::size_t i>(std::integral_constant<std::size_t, i>) {
+          if constexpr (i >= W && i < W * blocks_per_row) {
+            return moved[i - W];
+          } else {
+            return under_t(0);
+          }
+        });
+        data = not_moved | moved;
       }
     }
     template <coord d, bool check = true>
     constexpr board_t move() const {
-      checking_guard _(*this);
       board_t result = *this;
       result.move_<d, check>();
-      checking_guard _2(result);
       return result;
     }
     friend constexpr std::string to_string(const board_t &board) {
@@ -196,7 +185,6 @@ namespace reachability {
       return ret;
     }
     constexpr int clear_full_lines() {
-      checking_guard _(*this);
       auto board = data;
       constexpr int needed = std::numeric_limits<decltype(W)>::digits - std::countl_zero(W) - 1;
       static_for<needed>([&](auto i) {
@@ -228,85 +216,14 @@ namespace reachability {
     alignas(std::experimental::memory_alignment_v<data_t>) data_t data = 0;
     template <std::size_t N>
     static constexpr std::experimental::fixed_size_simd<under_t, N> zero = {};
-    template <int dx>
-    static constexpr data_t mask_move() {
-      board_t mask;
-      if constexpr (dx > 0) {
-        static_for<dx>([&](auto i) {
-          static_for<H>([&](auto j) {
-            mask.template set<i, j>();
-          });
-        });
-      } else if constexpr (dx < 0) {
-        static_for<-dx>([&](auto i) {
-          static_for<H>([&](auto j) {
-            mask.template set<W - 1 - i, j>();
-          });
-        });
-      }
-      return (~mask).data;
-    }
     static constexpr data_t mask_board() {
-      board_t ret;
-      ret.data = mask;
-      ret.data[last] = last_mask;
-      return ret.data;
-    }
-    template <int removed, bool from_right>
-    static constexpr data_t my_split(data_t data) {
-      if constexpr (removed == 0) {
-        return data;
-      } else if constexpr (from_right) {
-        auto [carry, _] = split<num_of_under-removed, removed>(data);
-        return to_fixed_size(concat(zero<removed>, carry));
-      } else {
-        auto [_, carry] = split<removed, num_of_under-removed>(data);
-        return to_fixed_size(concat(carry, zero<removed>));
-      }
-    }
-    template <int x_shift, int y_shift>
-    static constexpr data_t my_shift(data_t data) {
-      if constexpr (y_shift == lines_per_under || y_shift == -lines_per_under) {
-        data = data_t(0);
-      } else if constexpr (y_shift < 0) {
-        data >>= -y_shift * W;
-      } else if constexpr (y_shift > 0) {
-        data <<= y_shift * W;
-      }
-      if constexpr (x_shift > 0) {
-        data <<= x_shift;
-      } else if constexpr (x_shift < 0) {
-        data >>= -x_shift;
+      data_t data = {0};
+      data = ~data;
+      static_assert(H % block_size == 0);
+      for (std::size_t i = H / block_size * W; i < num_of_under; ++i) {
+        data[i] = 0;
       }
       return data;
     }
-    static constexpr under_t convert_to_under_t(std::string_view in) {
-      under_t res = 0;
-      for (char c : in) {
-        res *= 2;
-        if (c == 'X')
-          res += 1;
-      }
-      return res;
-    }
-    constexpr void check_invariant() const {
-      static_for<last>([&](auto i) {
-        if (data[i] & ~mask) {
-          std::unreachable();
-        }
-      });
-      if (data[last] & ~last_mask) {
-        std::unreachable();
-      }
-    }
-    struct checking_guard {
-      const board_t &board;
-      constexpr checking_guard(const board_t &board) : board(board) {
-        board.check_invariant();
-      }
-      constexpr ~checking_guard() {
-        board.check_invariant();
-      }
-    };
   };
 }
