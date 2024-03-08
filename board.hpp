f3e0dc37008149841e7e30ad3558b6688f21568f
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2024-03-08 15:57:39 +0800

    try intrinsics?

--- board.hpp
+++ board.hpp
@@ -10,6 +10,7 @@
 #include <cstdint>
 #include <bit>
 #include <experimental/simd>
+#include "immintrin.h"
 
 namespace reachability {
   template <unsigned W, unsigned H>
@@ -227,3 +228,197 @@ namespace reachability {
     }
   };
 }
+
+template <>
+struct reachability::board_t<10, 24> {
+  static constexpr int W = 10, H = 24;
+  static constexpr int block_size = 8;
+  static constexpr int blocks_per_row = 3;
+  using under_t = std::uint8_t;
+  static constexpr int num_of_under = 32;
+  constexpr board_t() {
+  }
+  constexpr board_t(std::string_view s): board_t(convert_to_array(s)) {
+  }
+  constexpr board_t(std::array<under_t, num_of_under> d): data{
+    d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9],
+    d[10], d[11], d[12], d[13], d[14], d[15], d[16], d[17], d[18], d[19],
+    d[20], d[21], d[22], d[23], d[24], d[25], d[26], d[27], d[28], d[29],
+    0, 0
+  } {
+  }
+  static constexpr std::array<under_t, num_of_under> convert_to_array(std::string_view s) {
+    std::string reversed = std::string(s.rbegin(), s.rend());
+    std::array<under_t, num_of_under> data = {0};
+    for (std::size_t y = 0; y < H; ++y) {
+      for (std::size_t x = 0; x < W; ++x) {
+        if (reversed[y * W + x] == 'X') {
+          data[y / block_size * W + x] |= under_t(1) << (y % block_size);
+        }
+      }
+    }
+    return data;
+  }
+  template <int x, int y>
+  constexpr void set() {
+    data[y / block_size * W + x] |= under_t(1) << (y % block_size);
+  }
+  template <int x, int y>
+  constexpr int get() const {
+    if ((x < 0) || (x >= W) || (y < 0) || (y >= H)) {
+      return 2;
+    }
+    return (data[y / block_size * W + x] >> (y % block_size)) & 1;
+  }
+  constexpr bool any() const {
+    return *this != board_t{};
+  }
+  constexpr bool operator!=(const board_t &other) const {
+    return _mm256_movemask_epi8(data != other.data);
+  }
+  constexpr board_t operator~() const {
+    board_t other;
+    other.data = mask_board & ~data;
+    return other;
+  }
+  constexpr board_t &operator&=(const board_t &rhs) {
+    data &= rhs.data;
+    return *this;
+  }
+  constexpr board_t operator&(const board_t &rhs) const {
+    board_t result = *this;
+    result &= rhs;
+    return result;
+  }
+  constexpr board_t &operator|=(const board_t &rhs) {
+    data |= rhs.data;
+    return *this;
+  }
+  constexpr board_t operator|(const board_t &rhs) const {
+    board_t result = *this;
+    result |= rhs;
+    return result;
+  }
+  constexpr board_t &operator^=(const board_t &rhs) {
+    data ^= rhs.data;
+    return *this;
+  }
+  constexpr board_t operator^(const board_t &rhs) const {
+    board_t result = *this;
+    result ^= rhs;
+    return result;
+  }
+  template <coord d, bool check = true>
+  constexpr void move_() {
+    constexpr int dx = d[0], dy = d[1];
+    if constexpr (dx != 0) {
+      data = permute<[](std::size_t i) -> std::size_t {
+        auto column = i / W, row = i % W;
+        if (column >= blocks_per_row) {
+          return num_of_under;
+        } else if (dx > 0 && row < dx) {
+          return num_of_under;
+        } else if (dx < 0 && row >= W + dx) {
+          return num_of_under;
+        } else {
+          return i - dx;
+        }
+      }>(data);
+    }
+    if constexpr (dy < 0) {
+      auto not_moved = data >> -dy;
+      auto moved = permute<[](std::size_t i) -> std::size_t {
+        if (i + W < W * blocks_per_row) {
+          return i + W;
+        } else {
+          return num_of_under;
+        }
+      }>(data << (block_size + dy));
+      data = not_moved | moved;
+    } else if constexpr (dy > 0) {
+      auto not_moved = data << dy;
+      auto moved = permute<[](std::size_t i) -> std::size_t {
+        if (i >= W && i < W * blocks_per_row) {
+          return i - W;
+        } else {
+          return num_of_under;
+        }
+      }>(data >> (block_size - dy));
+      data = not_moved | moved;
+    }
+  }
+  template <coord d, bool check = true>
+  constexpr board_t move() const {
+    board_t result = *this;
+    result.move_<d, check>();
+    return result;
+  }
+  friend constexpr std::string to_string(const board_t &board) {
+    std::string ret;
+    static_for<H>([&](auto y) {
+      std::string this_ret;
+      static_for<W>([&](auto x) {
+        this_ret += board.template get<x, y>() ? "[]" : "  ";
+      });
+      this_ret += '\n';
+      ret = this_ret + ret;
+    });
+    return ret;
+  }
+  friend constexpr std::string to_string(const board_t &board1, const board_t &board2) {
+    std::string ret;
+    static_for<H>([&](auto y) {
+      std::string this_ret;
+      static_for<W>([&](auto x) {
+        bool b1 = board1.template get<x, y>();
+        bool b2 = board2.template get<x, y>();
+        if (b1 && b2) {
+          this_ret += "%%";
+        } else if (b1) {
+          this_ret += "..";
+        } else if (b2) {
+          this_ret += "[]";
+        } else {
+          this_ret += "  ";
+        }
+      });
+      this_ret += '\n';
+      ret = this_ret + ret;
+    });
+    return ret;
+  }
+  friend constexpr std::string to_string(const board_t &board1, const board_t &board2, const board_t &board_3) {
+    std::string ret;
+    static_for<H>([&](auto y) {
+      std::string this_ret;
+      static_for<W>([&](auto x) {
+        bool tested[2] = {bool(board1.template get<x, y>()), bool(board2.template get<x, y>())};
+        bool b3 = board_3.template get<x, y>();
+        std::string symbols = "  <>[]%%";
+        for (int i = 0; i < 2; ++i) {
+          this_ret += symbols[b3 * 4 + tested[i] * 2 + i];
+        }
+      });
+      this_ret += '\n';
+      ret = this_ret + ret;
+    });
+    return ret;
+  }
+private:
+  typedef std::uint8_t data_t [[gnu::vector_size(32)]];
+  data_t data = {0};
+  template <std::size_t N>
+  static constexpr std::experimental::fixed_size_simd<under_t, N> zero = {};
+  static constexpr data_t mask_board = {
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0x00, 0x00
+  };
+  template <auto f>
+  static constexpr data_t permute(data_t data) {
+    return [=]<std::size_t... i>(std::index_sequence<i...>) {
+      return __builtin_shufflevector(data, data_t{0}, f(i)...);
+    }(std::make_index_sequence<32>());
+  }
+};
